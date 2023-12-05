f187dd490c141e6939e7388239bf73118e2d2ef9
Kravetskyi <nikita.kravecz@gmail.com>
2023-12-05 15:49:07 +0200

    Cart slide-in and slide-out

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,10 @@
 import * as model from "./model.js";
 import view from "./view.js";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlObserver = function () {
   model.observer(view.headerSection, view.nav);
 };
@@ -15,9 +19,25 @@ export const controlString = function (min, sec) {
   view.updateTimer(min, sec);
 };
 
+const controlCartOpen = function () {
+  view.cartOpen();
+};
+
+const controlCartClose = function () {
+  view.cartClose();
+};
+
+const controlPagination = function (bool = false) {
+  if (bool) view.movePrev();
+  else view.moveNext();
+};
+
 const init = () => {
   controlObserver();
   controlTimer();
+  view.addHandlerBtn(controlCartOpen);
+  view.addHandlerCartClose(controlCartClose);
+  view.addHandlerPagination(controlPagination);
 };
 
 init();
--- src/js/view.js
+++ src/js/view.js
@@ -6,6 +6,14 @@ class View {
   nav = document.querySelector(".nav-wrapper");
   _timer_seconds = document.querySelector(".timer__seconds");
   _timer_minutes = document.querySelector(".timer__minutes");
+  _cartBtn = document.getElementById("cart_btn");
+  _overlay = document.querySelector(".overlay");
+  _cart = document.querySelector(".cart");
+  _cartClose = document.querySelector(".cart__close");
+  _pagBtns = Array.from(
+    document.querySelector(".pagination").querySelectorAll("*")
+  );
+  _slider = document.querySelector(".slider");
 
   updateTimer(min, sec) {
     const arrayMinutes = Array.from(this._timer_minutes.querySelectorAll("*"));
@@ -29,6 +37,64 @@ class View {
       arraySeconds[0].innerHTML = 0;
       arraySeconds[1].innerHTML = secStr;
     }
+  }
+
+  addHandlerBtn(callback) {
+    this._cartBtn.addEventListener("click", (e) => {
+      const btn = e.target.closest("#cart_btn");
+      if (!btn) return;
+      callback();
+    });
+  }
+
+  addHandlerCartClose(callback) {
+    [this._overlay, this._cartClose].forEach((el) =>
+      el.addEventListener("click", (e) => {
+        const target1 = e.target.closest(".cart__close");
+        const target2 = e.target.closest(".overlay");
+        if (!target1 && !target2) return;
+        callback();
+      })
+    );
+  }
+
+  addHandlerPagination(callback) {
+    this._pagBtns.forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        const prev = e.target.closest(".previous");
+        const next = e.target.closest(".next");
+        if (!prev && !next) return;
+        if (prev) return callback(true);
+        callback();
+      })
+    );
+  }
+
+  cartOpen() {
+    this._overlay.classList.add("show");
+    this._cart.classList.add("show");
+    this._cart.classList.add("cart-slide-in");
+    this._cart.classList.remove("cart-slide-out");
+  }
+
+  cartClose() {
+    this._overlay.classList.remove("show");
+    // this._cart.classList.remove("show");
+    this._cart.classList.remove("cart-slide-in");
+    this._cart.classList.add("cart-slide-out");
+  }
+
+  movePrev() {
+    this._slider.classList.add("first-page");
+    this._slider.classList.remove("last-page");
+    this._pagBtns[0].classList.add("disabled");
+    this._pagBtns[1].classList.remove("disabled");
+  }
+  moveNext() {
+    this._slider.classList.remove("first-page");
+    this._slider.classList.add("last-page");
+    this._pagBtns[1].classList.add("disabled");
+    this._pagBtns[0].classList.remove("disabled");
   }
 }
 
